2c34af86bf650c2d2a51812d94099320dc70d3c7
Jaron-S <jaronschoor@gmail.com>
2024-02-16 11:22:49 -0700

    new banner ads

--- app/_mocks/data/carousel.ts
+++ app/_mocks/data/carousel.ts
@@ -1,34 +1,34 @@
 const carouselData = [
   {
     id: "1",
-    title: "Cushions & Throws",
-    url: "/cushions-throws",
-    imageUrl: "/images/carousel/scene1.png",
+    title: "Home Decor",
+    url: "/home-decor",
+    imageUrl: "/images/carousel/vase-banner.png",
   },
   {
     id: "2",
-    title: "Home Decor",
-    url: "/home-decor",
-    imageUrl: "/images/carousel/scene2.png",
+    title: "Cusions & Throws",
+    url: "/cushions-throws",
+    imageUrl: "/images/carousel/cushions-throws-banner.png",
   },
   {
     id: "3",
-    title: "Modern Lighting Solutions",
+    title: "Lighting Solutions",
     url: "/lighting",
-    imageUrl: "/images/carousel/scene3.png",
+    imageUrl: "/images/carousel/lighting-banner.png",
   },
-  {
-    id: "4",
-    title: "Modern Dinnerware",
-    url: "/dinnerware",
-    imageUrl: "/images/carousel/scene4.png",
-  },
-  {
-    id: "5",
-    title: "Modern Planters",
-    url: "/planters",
-    imageUrl: "/images/carousel/scene5.png",
-  },
+  // {
+  //   id: "4",
+  //   title: "Modern Dinnerware",
+  //   url: "/dinnerware",
+  //   imageUrl: "/images/carousel/scene4.png",
+  // },
+  // {
+  //   id: "5",
+  //   title: "Modern Planters",
+  //   url: "/planters",
+  //   imageUrl: "/images/carousel/scene5.png",
+  // },
 ];
 
 export default carouselData;
